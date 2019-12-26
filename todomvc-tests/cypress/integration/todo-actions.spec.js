4959ec348dd85fa000848a985509e02fcafc6e91
IvoDimitrov <ivopdimitrov@gmail.com>
2019-12-26 23:28:16 +0200

    Added page objects

---
+++ todomvc-tests/cypress/integration/todo-actions.spec.js
@@ -0,0 +1,40 @@
+/// <reference types="cypress" />
+import {
+  navigate,
+  addTodo,
+  validateTodoText,
+  toggleTodo,
+  clearCompleted,
+  validateTodoCompletedState,
+  validateToggleState,
+  validateNumberOfTodosShown
+} from "../page-objects/todo-page";
+
+describe("todo actions", () => {
+  beforeEach(() => {
+    navigate();
+
+    addTodo("Clean room");
+  });
+
+  it("should add a new todo to the list", () => {
+    validateTodoText(0, "Clean room");
+
+    validateToggleState(0, false);
+  });
+
+  describe("toggling todos", () => {
+    it("should toggle test correctly", () => {
+      toggleTodo(0);
+      validateTodoCompletedState(0, true);
+    });
+
+    it("should clear completed", () => {
+      toggleTodo(0);
+
+      clearCompleted();
+
+      validateNumberOfTodosShown(0);
+    });
+  });
+});
---
+++ todomvc-tests/cypress/integration/todo-filtering.spec.js
@@ -0,0 +1,40 @@
+/// <reference types="cypress" />
+import {
+  navigate,
+  addTodo,
+  toggleTodo,
+  showOnlyActiveTodos,
+  showOnlyCompletedTodos,
+  showAllTodos,
+  validateNumberOfTodosShown
+} from "../page-objects/todo-page";
+
+describe("filtering", function() {
+  beforeEach(() => {
+    navigate();
+
+    addTodo("Clean room");
+    addTodo("Learn JavaScript");
+    addTodo("Use Cypress");
+
+    toggleTodo(1);
+  });
+
+  it('should filter "Active" correctly', () => {
+    showOnlyActiveTodos();
+
+    validateNumberOfTodosShown(2);
+  });
+
+  it('should filter "Completed" correctly', () => {
+    showOnlyCompletedTodos();
+
+    validateNumberOfTodosShown(1);
+  });
+
+  it('should filter "All" correctly', () => {
+    showAllTodos();
+
+    validateNumberOfTodosShown(3);
+  });
+});
