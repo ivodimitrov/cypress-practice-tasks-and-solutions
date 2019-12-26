1c4c1582bd181c3d443274030d0ca4fb996304e2
IvoDimitrov <ivopdimitrov@gmail.com>
2019-12-26 20:56:17 +0200

    Added third test

---
+++ todomvc-tests/cypress/integration/todomvc_third.spec.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+
+it('should add a new todo to the list', () => {
+  cy.visit('http://todomvc-app-for-testing.surge.sh/')
+
+  cy.get('.new-todo', {timeout: 6000}).type('Clean room{enter}')
+
+  cy.get('label').should('have.text', 'Clean room')
+
+  // cy.get('.toggle').click()
+
+  // cy.contains('Clear completed').click()
+})
